05ecc2ebf755b4aafc7ba897a82711f304338559
yuxp <1039354317@qq.com>
2017-04-11 17:13:17 +0800

    登录页完善

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var imagemin = require('gulp-imagemin');
 var cssmin = require('gulp-clean-css');
-
-
+var livereload = require('gulp-livereload'); 
 
 gulp.task('less', function(){
     gulp.src('src/less/*')
@@ -41,12 +40,22 @@ gulp.task('default', function(){
     gulp.run('lint', 'sass', 'scripts');
 
     
-});
+}); 
+
 
 gulp.task('watch',function(){
-    gulp.watch('src/less/*', function(){
-            gulp.run('less');
+
+    gulp.watch('src/less/*.less', ['less']);
+    livereload.listen();//这里需要注意！旧版使用var server = livereload();已经失效    
+    // app/**/*.* 的意思是 app 文件夹下的 任何文件夹 的 任何文件  
+    gulp.watch('*.html', function(event) {  
+        livereload.changed(event.path);  
     });
+    gulp.watch('src/less/*.less', function(event) {  
+        livereload.changed(event.path);  
+    });  
 })
+
+
 
 
